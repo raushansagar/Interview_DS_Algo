05e34df556ba39cab3046f564b82b90561016601
RaushanSagar1694 <dayasagar1694@gmail.com>
2026-04-07 22:40:21 +0530

    add link

---
+++ Bit Manipulation/3133. Minimum Array End.java
@@ -0,0 +1,21 @@
+/*
+    3133. Minimum Array End 
+    Leetcode Link :- https://leetcode.com/problems/minimum-array-end/submissions/1971813890/
+ */
+
+
+// Approch 1
+// Time complexity O(n)
+
+
+class Solution {
+    public long minEnd(int n, int x) {
+        long num = x;
+
+        for (int i = 1; i < n; i++) {
+            num = (num + 1) | x;
+        }
+
+        return num;
+    }
+}
