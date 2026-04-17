0736214ecae4db93bb8d06d707da6d78aaab253f
RaushanSagar1694 <dayasagar1694@gmail.com>
2026-04-17 23:26:10 +0530

    add link

---
+++  Stack And Queue/Monotonic Stack/42. Trapping Rain Water.java
@@ -0,0 +1,38 @@
+/*
+    42. Trapping Rain Water
+    Leetcode Link :- https://leetcode.com/problems/trapping-rain-water/description/
+*/
+
+
+
+// Approch 1
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        // left
+        int[] left = new int[n];
+        left[0] = height[0];
+
+        // right
+        int[] right = new int[n];
+        right[n-1] = height[n-1];
+
+        for(int i = 1; i < n; i++){
+            left[i] = Math.max(left[i-1], height[i]);
+        }
+        for(int i = n-2; i >= 0; i--){
+            right[i] = Math.max(right[i+1], height[i]);
+        }
+
+        int maxWater = 0;
+        for(int i = 0; i < n; i++){
+            int currWater =  Math.min(left[i], right[i]) - height[i];
+            if(currWater > 0) maxWater += currWater;
+        }
+
+
+        return maxWater;
+    }
+}
