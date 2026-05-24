0fe52fbb09faf58d674678dd5397bce2de6e5bc0
RaushanSagar1694 <dayasagar1694@gmail.com>
2026-05-24 22:50:22 +0530

    add link

--- Graph/MinimumSpaningTree & DisjointSet Problem/DisjointSetUnion.java
+++ Graph/MinimumSpaningTree & DisjointSet Problem/DisjointSetUnion.java
@@ -4,6 +4,10 @@
 
 
 
+
+ 
+// DSU by Rank
+
 public class DisjointSetUnion {
 
     // find and union by path compression
@@ -33,6 +37,62 @@ public class DisjointSetUnion {
     }
 
     public static void main(String[] args) {
+        int n = 4;
 
+        int[] rank = new int[n];
+        int[] parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rank[i] = 0;
+            parent[i] = i;
+        }
+    }
+}
+
+
+
+
+// DSU By Size
+
+class DisjointSetUnionSize{
+
+    // find and union by path compression
+    public int find(int x, int[] parent) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x], parent);
+    }
+
+    // union 
+    public void union(int x, int y, int[] parent, int[] size) {
+
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+
+        if(x_parent == y_parent) return;
+
+        if(size[x_parent] > size[y_parent]){
+            parent[y_parent] = x_parent;
+            size[x_parent] += size[y_parent];
+        }
+        else if(size[y_parent] > size[x_parent]){
+            parent[x_parent] = y_parent;
+            size[y_parent] += size[x_parent];
+        }
+        else{
+            parent[y_parent] = x_parent;
+            size[x_parent] += size[y_parent];
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        int[] size = new int[n];
+        int[] parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            size[i] = 1;
+            parent[i] = i;
+        }
     }
 }
