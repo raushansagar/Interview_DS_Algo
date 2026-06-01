2b4184364d0516d49acf96ccd52b8490a3ff26f1
RaushanSagar1694 <dayasagar1694@gmail.com>
2026-06-01 14:02:56 +0530

    add link

---
+++ Graph/MinimumSpaningTree & DisjointSet Problem/721. Accounts Merge.java
@@ -0,0 +1,151 @@
+/*
+    721. Accounts Merge
+ */
+
+
+
+
+
+// DFS 
+class Solution {
+    public void DFS(int u, List<List<String>> accounts, boolean[] vis, Set<String> set, int n){
+        
+        vis[u] = true;
+
+        for(int i = 1; i < accounts.get(u).size(); i++){
+            String s = accounts.get(u).get(i);
+            set.add(s);
+        }
+
+        for(int v = 0; v < n; v++){
+            if(vis[v]) continue;
+
+            for(int i = 1; i < accounts.get(v).size(); i++){
+                String str = accounts.get(v).get(i);
+
+                if(set.contains(str)){
+                    DFS(v, accounts, vis, set, n);
+                }
+            }
+        }
+
+    }
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        
+        int n = accounts.size();
+        boolean[] vis = new boolean[n];
+
+        List<List<String>> ans = new ArrayList<>();
+
+        for(int u = 0; u < n; u++){
+            Set<String> set = new HashSet<>();
+
+            if(!vis[u]){
+
+                String name = accounts.get(u).get(0);
+                DFS(u, accounts, vis, set, n);
+
+                LinkedList<String> list = new LinkedList<>(set);
+                Collections.sort(list);
+
+                list.addFirst(name);
+                ans.add(list);
+            }
+        }
+        
+
+        return ans;
+    }
+}
+
+
+
+
+// DSU
+
+class Solution {
+
+    // find and union by path compression
+    public int find(int x, int[] parent){
+        if(parent[x] == x) return x;
+
+        return parent[x] = find(parent[x], parent);
+    }
+
+    public void union(int x, int y, int[] rank, int[] parent){
+
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+
+        if(x_parent == y_parent) return;
+
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if(rank[y_parent] > rank[x_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else{
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        
+        int V = accounts.size();
+
+        int[] rank = new int[V];
+        int[] parent = new int[V];
+
+        for(int i = 0; i < V; i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int x = 0; x < V; x++){
+            for(int j = 1; j < accounts.get(x).size(); j++){
+                String s = accounts.get(x).get(j);
+
+                if(map.containsKey(s)){
+                    int y = map.get(s);
+
+                    int y_parent = find(y, parent);
+                    int x_parent = find(x, parent);
+                    
+                    if(x_parent == y_parent) continue;
+
+                    union(y, x, rank, parent);
+                }
+                else{
+                    map.put(s, x);
+                }
+            }
+        }
+
+        Map<Integer, LinkedList<String>> list = new HashMap<>();
+
+        for(String key : map.keySet()){
+            int x = map.get(key);
+            int x_parent = find(x, parent);
+
+            list.putIfAbsent(x_parent, new LinkedList<>());
+            list.get(x_parent).addLast(key);
+        }
+
+        List<List<String>> ans = new ArrayList<>();
+
+        for(int key : list.keySet()){
+            String name = accounts.get(key).get(0);
+
+            LinkedList<String> temp = list.get(key);
+            Collections.sort(temp);
+            temp.addFirst(name);
+
+            ans.add(temp);
+        }
+
+        return ans;
+    }
+}
